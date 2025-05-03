364de24d9b27eb2e9d157c418592d0f22b86db17
Munasinghe123 <jsan39196.com>
2025-05-04 01:47:24 +0530

    console.log("VITE_BACKEND_URL =", process.env.VITE_BACKEND_URL);

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+console.log("VITE_BACKEND_URL =", process.env.VITE_BACKEND_URL); 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
